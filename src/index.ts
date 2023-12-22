e6913fb1ccd48691ae4bbc75dd03c0574af57dea
Alex Andru <alex007d@gmail.com>
2023-12-22 20:17:12 +0100

    Add /Ping command to bot

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
 import "dotenv/config";
+import { Client, Events, GatewayIntentBits } from "discord.js";
 import logger from "./logger/logger.js";
+import "./commands/commands.js";
+
+const discordBotToken = process.env.DISCORD_TOKEN;
 
 const log = logger("app");
 
 log.info("Launching Discord Remindabot");
 
-log.success("Client launched succesfully");
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.GuildMembers,
+  ],
+});
 
-log.error("Shutting down Remindabot...");
+client.once(Events.ClientReady, (readyClient) => {
+  log.success(
+    `Client logged in succesfully. Logged in as ${readyClient.user.tag}`,
+  );
+});
+
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isChatInputCommand()) {
+    return;
+  }
+  await interaction.reply("Krisa Krisa ;)");
+
+  console.log(interaction);
+});
+
+await client.login(discordBotToken);
